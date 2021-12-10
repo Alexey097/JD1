1b7f5498cb16979852b8a6013b4d690e4e4c7527
Alexey097 <alex_su16@mail.ru>
2021-12-11 00:27:04 +0300

    Четвертый

---
+++ src/HomeWork_1/Task6_1.java
@@ -0,0 +1,21 @@
+package HomeWork_1;
+
+public class Task6_1 {
+    public static String createPhoneNumber(int[] a) {
+        System.out.print("(");
+        System.out.print(a[0]);
+        System.out.print(a[1]);
+        System.out.print(a[2]);
+        System.out.print(") ");
+        System.out.print(a[3]);
+        System.out.print(a[4]);
+        System.out.print(a[5]);
+        System.out.print("-");
+        System.out.print(a[6]);
+        System.out.print(a[7]);
+        System.out.print(a[8]);
+        System.out.print(a[9]);
+        String dot = ".";
+        return dot;
+    }
+}
