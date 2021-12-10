08caa65d43db969cf20d643250f61a4badf07f77
Alexey097 <alex_su16@mail.ru>
2021-12-10 23:59:57 +0300

    Третий

---
+++ src/HomeWork_1/Task6.java
@@ -0,0 +1,10 @@
+package HomeWork_1;
+
+public class Task6 {
+    public static void main(String[] args) {
+        int [] arrayOfNumbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        System.out.println(arrayOfNumbers);
+
+
+    }
+}
