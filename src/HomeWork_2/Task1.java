0fbb9e45176c8bcfda9fa22404178af1e23d8ab3
Alexey097 <alex_su16@mail.ru>
2021-12-14 01:31:42 +0300

    HomeWork#2

--- src/HomeWork_2/Task1.java
+++ src/HomeWork_2/Task1.java
@@ -6,7 +6,6 @@ public class Task1 {
     public static void main(String[] args) {
         int povtor = 0;
         int countOfOdds = 0;
-        float result;
 
         while (povtor<10000) {
             Random rnd = new Random();
@@ -14,12 +13,9 @@ public class Task1 {
             if (chislo%2==0){
                 countOfOdds++;
             }
-//            System.out.println(chislo);
             povtor++;
 
         }
-//        System.out.println(povtor);
-//        System.out.println(countOfOdds);
         float a = (float) countOfOdds/100;
         System.out.println("Шанс получения четного числа из 10000 псевдорандомных чисел равен "+a+"%");
     }
--- src/HomeWork_2/Task2.java
+++ src/HomeWork_2/Task2.java
@@ -5,34 +5,26 @@ import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
+        //Вводим число, числа которого нужно сложить
         Scanner in = new Scanner(System.in);
         System.out.println("Введите число");
         int chislo = in.nextInt();
-        int dublicatChisla = chislo;
-        int kolvo = 0;
-        while (dublicatChisla > 0) {
-            int partNumber = dublicatChisla % 10;
-            //System.out.println(chislo);
-            kolvo++;
-            dublicatChisla /= 10;
-        }
-        //System.out.println(kolvo);
-        //int[] odd = new int[kolvo];
-        //int[] even = new int[kolvo];
-
+        //Задаю переменные куда буду помещать числа в зависимости от того четное оно или нет
         int odd = 0;
         int even = 0;
-        //System.out.println(chislo);
+        //В цикле отделяю справа налево числа и проверяю в каждой итерации четное число или нечетное
         while (chislo > 0) {
             int partNumber = chislo % 10;
-            //System.out.println(chislo);
             if (partNumber % 2 == 0) {
+                //Если четное - число прибавляю в переменную четных
                 odd += partNumber;
             } else {
+                //Если нечетное - число прибавляю в переменную нечетных
                 even += partNumber;
             }
             chislo /= 10;
         }
+        //Вывожу суммы в консоль
         System.out.println("Сумма четных цифр в числе равна "+odd);
         System.out.println("Сумма нечетных цифр в числе равна "+even);
     }
