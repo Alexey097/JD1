e880ed7c226a5228c6a67191444a2d77904eb7ba
Alexey097 <alex_su16@mail.ru>
2021-12-22 01:30:21 +0300

    HomeWork#2 Циклы и Массивы

--- src/HomeWork2/arrays/Task2_2doWhile.java
+++ src/HomeWork2/arrays/Task2_2doWhile.java
@@ -4,28 +4,36 @@ import HomeWork2.utils.ArraysUtilsTask2_1;
 
 public class Task2_2doWhile {
     public static void main(String[] args) {
-        doWhileTask();
+
+        doWhileTask1();
+        doWhileTask2();
+        doWhileTask3();
     }
-    public static void doWhileTask(){
+    public static void doWhileTask1() {
         int[] doWhile = ArraysUtilsTask2_1.arrayFromConsole();
         int i = 0;
 
         do {
-            System.out.print(doWhile[i]+" ");
+            System.out.print(doWhile[i] + " ");
             i++;
-        } while (i<doWhile.length);
-        i=1;
+        } while (i < doWhile.length);
         System.out.println("");
         System.out.println("-------------------------");
-
+    }
+    public static void doWhileTask2() {
+        int[] doWhile = ArraysUtilsTask2_1.arrayFromConsole();
+        int i = 1;
         do {
-            System.out.print(doWhile[doWhile.length-(doWhile.length-i)]+" ");
-            i+=2;
+            System.out.print(doWhile[doWhile.length - (doWhile.length - i)] + " ");
+            i += 2;
 
-        }while (i<doWhile.length);
+        } while (i < doWhile.length);
         System.out.println("");
         System.out.println("-------------------------");
-        i=1;
+    }
+    public static void doWhileTask3(){
+        int[] doWhile = ArraysUtilsTask2_1.arrayFromConsole();
+        int i = 1;
 
         do {
             System.out.print(doWhile[doWhile.length-i]+" ");
---
+++ src/HomeWork2/arrays/Task2_4.java
@@ -0,0 +1,86 @@
+package HomeWork2.arrays;
+
+import HomeWork2.utils.ArraysUtilsTask2_1;
+
+public class Task2_4 {
+    public static void main(String[] args) {
+        twoFourSix246();
+    }
+
+    public static void twoFourOne241() {
+        int k = 0;
+        int[] array = ArraysUtilsTask2_1.arrayRandom(5, 99);
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 0) {
+                if (array[i] % 2 == 0) {
+                    k += array[i];
+                }
+            }
+        }
+        System.out.println(k);
+    }
+
+    public static void twoFourTwo242() {
+        int[] array = ArraysUtilsTask2_1.arrayRandom(5, 99);
+        int max = -1;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                if (i % 2 == 0) {
+                    max = array[i];
+                }
+            }
+        }
+        System.out.println(max + "m");
+    }
+
+    public static void twoFourThree243() {
+        int[] array = ArraysUtilsTask2_1.arrayRandom(5, 99);
+        int averageSum = 0;
+        for (int i = 0; i < array.length; i++) {
+            averageSum += array[i];
+        }
+        averageSum /= array.length;
+        for (int j = 0; j < array.length; j++) {
+            if (array[j] < averageSum) {
+                System.out.println(array[j] + "m");
+            }
+        }
+    }
+
+    public static void twoFourFour244() {
+        int[] array = ArraysUtilsTask2_1.arrayRandom(5, 99);
+        int min1 = array[0];
+        int min2 = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (min1 > array[i]) {
+                min1 = array[i];
+            }
+        }
+        for (int j = 0; j < array.length; j++) {
+            if (min2 > array[j] && array[j] != min1) {
+                min2 = array[j];
+            }
+        }
+        System.out.println(min1 + "   " + min2);
+    }
+
+    public static void twoFourFive245() {
+        int[] array = ArraysUtilsTask2_1.arrayRandom(5, 99);
+    }
+    public static void twoFourSix246() {
+        int[] array = ArraysUtilsTask2_1.arrayRandom(5, 199);
+        int sum = 0;
+        for (int i=0;i<array.length;i++){
+            int x = array[i];
+            while(array[i]>0){
+                sum += array[i]%10;
+                array[i]/=10;
+            }
+        array[i]=x;
+        }
+        System.out.println(sum);
+        for (int i : array) {
+            System.out.print(i+" ");
+        }
+}
+}
